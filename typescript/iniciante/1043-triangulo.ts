2b5449824f152fa0d5e4dd239d65950205cd16d8
Lancellot <assis.neto@rede.ulbra.br>
2026-03-02 18:21:18 -0300

    solve: problem 1043

---
+++ typescript/iniciante/1043-triangulo.ts
@@ -0,0 +1,15 @@
+import * as fs from "node:fs";
+
+const input = fs.readFileSync(0, "utf8").trim().split(/\s+/).map(Number);
+
+const a: number = input[0]!;
+const b: number = input[1]!;
+const c: number = input[2]!;
+
+const triangulo = (a: number, b: number, c: number): string => {
+
+    const isTriangulo = (a < b + c) && (b < a + c) && (c < a + b);
+    return isTriangulo ? "Perimetro = " + (a + b + c) : "Area = " + ((a + b) * c / 2).toFixed(1);
+}
+
+console.log(triangulo(a, b, c));
