5f20d99e95e8db9e3c3d7f8734dfcea176096360
Lancellot <assis.neto@rede.ulbra.br>
2026-03-23 10:04:46 -0300

    solve: problem 1060

--- typescript/iniciante/1060-numeros-positivos.ts
+++ typescript/iniciante/1060-numeros-positivos.ts
@@ -5,7 +5,7 @@ const input = fs.readFileSync(0, "utf8").trim().split(/\s+/).map(Number);
 let positivos : number = 0;
 
 for (let i = 0; i < input.length; i++) {
-    input[i] > 0 ? positivos++ : null;
+    input[i]! > 0 ? positivos++ : null;
 }
 
 console.log(`${positivos} valores positivos`);
