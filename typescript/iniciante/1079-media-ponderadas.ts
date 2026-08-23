f38ecd48370d449408f5bbcd781e3faed642f0da
Lancellot <assis.neto@rede.ulbra.br>
2026-08-23 16:52:01 -0300

    solve: problem 1079

---
+++ typescript/iniciante/1079-media-ponderadas.ts
@@ -0,0 +1,19 @@
+import * as fs from "node:fs";
+
+const input = fs.readFileSync(0, "utf8").trim().split("\n");
+
+const n = Number(input[0]);
+
+const mediaPonderada = (valores: number[][]): number[]  => {
+    const resultados: number[] = [];
+    for (let i = 0; i < n; i++) {
+        const [a, b, c] = valores[i]!;
+        const media = (a! * 2 + b! * 3 + c! * 5) / 10;
+        resultados.push(media);
+    }
+
+    return resultados;
+};
+
+const valores: number[][] = input.slice(1).map(linha => linha.split(" ").map(Number));
+const medias = mediaPonderada(valores);
